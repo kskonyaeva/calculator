5b011999b34fe36c5d9fcd2e576804421f1fd1d5
kskonyaeva <kskonyaeva@gmail.com>
2023-09-15 15:32:52 +0500

    calculator

---
+++ main.js
@@ -0,0 +1,75 @@
+const display = document.querySelector(".js-display");
+const btnsCalculate = document.querySelectorAll(".js-btn-calculate");
+const btns = document.querySelectorAll(".js-btn");
+
+console.log("btnsCalculate: ", btnsCalculate);
+console.log(btns);
+
+btns.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    //получение значений кнопок
+    let itemText = e.target.textContent;
+
+    //замена визуальных значков
+    if (itemText === "x") {
+      itemText = "*";
+    }
+    if (itemText === "÷") {
+      itemText = "/";
+    }
+    if (itemText === "AC") {
+      display.value = "";
+    }
+    if (itemText === "C") {
+      display.value = display.value.slice(0, -1);
+    }
+    if (itemText !== "C" && itemText !== "AC" && itemText !== "=") {
+      display.value += itemText;
+    }
+    if (itemText === "=") {
+      display.value = eval(display.value);
+    }
+
+    console.log(itemText);
+  });
+});
+
+//работа с sin, cos, tan ...
+const sin = () => {
+  display.value = Math.sin(display.value);
+};
+const cos = () => {
+  display.value = Math.cos(display.value);
+};
+const tan = () => {
+  display.value = Math.tan(display.value);
+};
+const pow = () => {
+  display.value = Math.pow(display.value, 2);
+};
+const sqrt = () => {
+  display.value = Math.sqrt(display.value, 2);
+};
+
+btnsCalculate.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    
+    let itemText = e.target.textContent;
+    if (itemText === "sin") {
+      sin();
+    }
+    if (itemText === "cos") {
+      cos();
+    }
+    if (itemText === "tan") {
+      tan();
+    }
+    if (itemText === "x²") {
+      pow();
+    }
+    if (itemText === "√") {
+      sqrt();
+    }
+
+  });
+});
